3051df12f409cf9d7a18f2a8f33a2a4321d152da
wenzzzel <wenzzzel@gmail.com>
2021-08-08 18:36:44 +0200

    Added highscore model and controller

---
+++ Controllers/HighScoreController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace core_rest_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HighScoreController : ControllerBase
+    {
+        private static readonly string[] UserNames = new[]
+        {
+            "wenzzzel", "davwen", "Allyer", "Dailiesst", "Geniusys", "PlotCampy", "ShiyaPeach", "Tinnysve", "FamousBorn"
+        };
+
+        private readonly ILogger<HighScoreController> _logger;
+
+        public HighScoreController(ILogger<HighScoreController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<HighScore> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new HighScore
+            {
+                Date = DateTime.Now.AddDays(index),
+                HighScore = rng.Next(-20, 55),
+                UserName = UserNames[rng.Next(UserNames.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ HighScore.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace core_rest_api
+{
+    public class HighScore
+    {
+        public string UserName { get; set; }
+        public DateTime Date { get; set; }
+        public int Score { get; set; }
+    }
+}
