9004c78f50f9494d31735e62fb200e8ad540c598
wenzzzel <wenzzzel@live.se>
2021-08-13 14:37:08 +0200

    Minor change

--- MyDbContext.cs
+++ MyDbContext.cs
@@ -9,15 +9,5 @@ namespace core_rest_api{
         public MyDbContext(DbContextOptions<MyDbContext> options){
 
         }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        if (!optionsBuilder.IsConfigured)
-        {
-            optionsBuilder.UseSqlServer("Server=tcp:wenzzzel.database.windows.net,1433;Initial Catalog=core_rest_api;Persist Security Info=False;User ID=wenzzzel;Password=N3r7$vsNxA;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-        }
- 
-    }
-
     }
 }
