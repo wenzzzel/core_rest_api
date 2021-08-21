7186c7670d3af4f417fc99ea5bfb0ed9107bf193
wenzzzel <wenzzzel@live.se>
2021-08-21 21:39:33 +0200

    Added UserLoginRequest model

---
+++ Models/DTOs/Requests/UserLoginRequest.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace core_rest_api.Models.DTOs.Requests{
+    public class UserLoginRequest{
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
